177c3702213fb42d4cd3cc72539444664b5cd495
Grant Brown <gnb225@nyu.edu>
2022-12-05 14:57:08 -0500

    Added font awesome, completed preliminary logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCloudBolt } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Counter />
-        
+        {/* <Counter /> */}
+        <h1 className="logo">timely <FontAwesomeIcon icon={faCloudBolt} color="#20BEE1" /> weather</h1>
       </header>
     </div>
   );
